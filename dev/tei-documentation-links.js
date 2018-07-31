874f7da91bafdc13597cd50493183550431e7104
hou2zi0 <max.gruentgens@adwmainz.de>
2018-07-31 18:38:20 +0200

    Pushed line numbering to live.

--- dev/tei-documentation-links.js
+++ dev/tei-documentation-links.js
@@ -199,11 +199,9 @@ function teiDocLinks() {
             return line.length > 0
           })
           .map((line, index) => {
-            console.log(line.length);
             return `<span class="line_numbering">${index+1}</span><span class="code_line">${line}</span>`;
           })
           .join('\n');
-        console.log(node.parentNode);
         node.parentNode.setAttribute('style', 'padding-left: 35px;');
       } else {
         snippet = newSnippetCommStr;
