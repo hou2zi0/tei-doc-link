d8517ce13eb11351293062b57300a2fb6d30dba0
hou2zi0 <max.gruentgens@adwmainz.de>
2018-08-01 21:13:56 +0200

    Updated dev & live.

--- JS/tei-documentation-links.js
+++ JS/tei-documentation-links.js
@@ -204,12 +204,8 @@ function teiDocLinks() {
         node.parentNode.setAttribute('style', 'padding: 10px 15px 10px 35px;');
       } else {
         snippet = newSnippetCommStr;
-        console.log(node.getAttribute('class')
-          .includes('linenumbers'));
         if (node.getAttribute('class')
-          .includes('has-linenumbers')) {
-          console.log(node.getAttribute('class')
-            .includes('has-linenumbers'));
+          .includes('linenumbers')) {
           node.parentNode.setAttribute('style', 'padding: 0px 15px 0px 35px;');
         }
       }
